f9f9c0d54d676c740b6d337597820482ec2e6582
amir nadiv <amir.nadiv@locusview.com>
2022-08-23 10:22:46 +0300

    Initial commit - 23.08.2022

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 app.get('/ping', (request, response) => {
     response.send('pong');
 });
+
 // Test
+
 app.get('/status', (request, response) => {
     response.send('TimeStamp');
 });
